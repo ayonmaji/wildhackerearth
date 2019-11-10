974bedadf163c8f3dc19f7be121bf4b2af3d19e7
ayonmaji <57018409+ayonmaji@users.noreply.github.com>
2019-11-10 12:37:06 +0530

    Adding assets

---
+++ admin/includes/footer.php
@@ -0,0 +1,17 @@
+
+                </footer>
+<!-- Footer -->
+
+    <footer class="py-5 bg-dark">
+      <div class="container">
+      	<a class="m-0 text-center text-white" href="/contact-us.php">Contact Us</a>
+			<a> | </a>
+			<a class="m-0 text-center text-white" href="">Privacy-Policy</a>
+			<a> | </a>
+			<a class="m-0 text-center text-white" href="">Terms & Condition</a>
+			<a> | </a>
+
+        <p class="m-0 text-center text-white">Copyright &copy; WildHackerEarth 2019</p>
+      </div>
+      <!-- /.container -->
+    </footer>
---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<!-- <div style="margin-top:40px">
+<footer class="w3-container w3-light-grey w3-center w3-bottom w3-small">
+		<p>
+			<a class="w3-text-theme w3-hover-opacity" href="about.php">Contact Us</a>
+			<a> | </a>
+			
+			<a class="w3-text-theme w3-hover-opacity" href="">Privacy-Policy</a>
+			<a> | </a>
+			<a class="w3-text-theme w3-hover-opacity" href="">Terms & Condition</a>
+			<a> | </a>
+			
+		</p>
+	</footer>
+</div> -->
+
+<!-- Footer -->
+
+</footer>
+<!-- Footer -->
+
+    <footer class="py-5 bg-dark">
+      <div class="container">
+      	<a class="m-0 text-center text-white" href="/contact-us.html">Contact Us</a>
+			<a> | </a>
+			<a class="m-0 text-center text-white" href="">Privacy-Policy</a>
+			<a> | </a>
+			<a class="m-0 text-center text-white" href="">Terms & Condition</a>
+			<a> | </a>
+
+        <p class="m-0 text-center text-white">Copyright &copy; WildHackerEarth 2019</p>
+      </div>
+      <!-- /.container -->
+    </footer>
